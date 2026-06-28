5e024d1eabe117b805a72d84c06460edad2fcc9b
KiranGolada <kirangolada9@gmail.com>
2026-06-28 12:48:28 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -4,7 +4,7 @@
     //     // TO MAKE THE MAP APPEAR YOU MUST
     //     // ADD YOUR ACCESS TOKEN FROM
     //     // https://account.mapbox.com
-    //     //accessToken: 'pk.eyJ1IjoiZGVsdGEtc3R1ZHVlbnQiLCJhIjoiY2xvMDk0MTVhMTJ3ZDJrcGR5ZDFkaHl4ciJ9.Gj2VU1wvxc7rFVt5E4KLOQ',
+    //     //accessToken: 'pk.eyJ1IjoiZGVsdGEtc3R1ZHVlbnQiLCJhIjoiY2xvMDk0MTVhMTJ3ZDJrGR5ZDFkaHl4ciJ9.Gj2VU1wvxc7rFVt5E4KLOQ',
     //     container: 'map', // container ID
     //     center: [77.209, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
     //     zoom: 9 // starting zoom
